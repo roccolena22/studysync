51cf510680b98c5c873c21b87364c20b023238c3
Rocco Lena <128648624+roccolena22@users.noreply.github.com>
2023-11-16 23:43:47 +0100

    .

--- src/user/hooks/getUser.jsx
+++ src/user/hooks/getUser.jsx
@@ -2,5 +2,13 @@ import { getFromLocalStorage } from "./localStorageHooks";
 
 export function getUser(email) {
   const users = getFromLocalStorage("users", []);
-  return users.find((user) => user.email === email);
+
+  if (users && users.length > 0) {
+    const user = users.find((user) => user.email === email);
+
+    if (user && user.email) {
+      return user;
+    }
+  }
+  return null;
 }
