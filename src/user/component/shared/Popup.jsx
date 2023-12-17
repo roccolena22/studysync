ae98e36658a32e16973f0240e2143e498e130e2e
Rocco Lena <roccolena@tuta.io>
2023-12-17 23:07:28 +0100

    .

--- src/user/component/shared/Popup.jsx
+++ src/user/component/shared/Popup.jsx
@@ -4,17 +4,19 @@ import Title from "./Title";
 export default function Popup({ children, handleClose, title }) {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-black bg-opacity-20 z-[110]">
-      <div className="bg-white rounded-lg px-4 w-5/6 md:w-2/3 h-5/6 overflow-y-auto">
-        <div className="flex justify-end pt-2 sticky top-0">
+      <div className="bg-white rounded-lg px-4 w-5/6 lg:w-2/3 h-5/6 overflow-y-auto">
+        <div className="flex justify-end pt-2 sticky top-0 bg-white">
           <Icon
             name="close"
-            style="rounded-2xl w-6 h-6 bg-white border hover:border-red-800 hover:text-red-800"
+            style="rounded-2xl w-6 h-6 hover:border border-red-800 hover:text-red-800"
             onClick={handleClose}
           />
         </div>
-        <div className="py-6">
+        <div className="sticky top-8 bg-white">
           {title && <Title title={title} fontSize="text-lg" />}
-          {children}
+        </div>
+        <div className="py-6">
+        {children}
         </div>
       </div>
     </div>
