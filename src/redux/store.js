54c75b6fab8e40aded89cd2fddf726cb843ff603
Rocco Lena <roccolena@tuta.io>
2024-01-04 12:15:24 +0100

    persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
-import followersReducer from './followersSlice';
-import usersReducer from './usersSlice';
-import eventsReducer from './eventsSlice';
-import bookingsReducer from './bookingsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { thunk } from 'redux-thunk';
+import authReducer from './slices/authSlice';
+import followersReducer from './slices/followersSlice';
+import eventsReducer from './slices/eventsSlice';
+import bookingsReducer from './slices/bookingsSlice';
+import usersReducer from './slices/usersSlice';
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+export const store = configureStore({
+  reducer:
+  {
+    auth: persistedReducer,
     followers: followersReducer,
     users: usersReducer,
     events: eventsReducer,
     bookings: bookingsReducer,
   },
-});
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
+})
+
+export const persistor = persistStore(store)
 
 export default store;
