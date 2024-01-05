3f0fb9577875ec7b3e32c2dc517798bc28a62dbf
Rocco Lena <roccolena@tuta.io>
2024-01-05 17:49:37 +0100

    .

--- src/guest/component/FullPageContainer.jsx
+++ src/guest/component/FullPageContainer.jsx
@@ -1,9 +1,18 @@
+import Slogan from "./Slogan";
+
 export default function FullPageContainer({ children }) {
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <div className="bg-white px-8 py-2 rounded-lg shadow-md w-full sm:w-3/5 lg:w-2/5 z-10">
-        {children}
+      <div className="sm:w-3/5 lg:w-2/5">
+        <Slogan
+          firstPart="Sync Your Learning with"
+          highlightedPart="StudySync"
+          secondPart="Build lessons, study sessions, and more together, connecting students and teachers on one smart platform." />
+        <div className="bg-white px-8 py-2 rounded-lg shadow-md w-full z-10">
+          {children}
+        </div>
       </div>
+
     </div>
   );
 }
---
+++ src/guest/component/Slogan.jsx
@@ -0,0 +1,11 @@
+export default function Slogan({firstPart, highlightedPart, secondPart}) {
+    return (
+        <div class="text-2xl font-semibold italic text-center text-white py-4">
+{firstPart}
+            <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-cyan-800 relative inline-block text-white">
+                <span class="relative text-white">{highlightedPart}</span>
+            </span>
+{secondPart}
+        </div>
+    )
+}
