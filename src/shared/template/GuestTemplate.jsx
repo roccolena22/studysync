f2c862cb3f0120278c9875c6cfc4a47e439f99de
Rocco Lena <roccolena@tuta.io>
2024-01-31 10:34:23 +0100

    .

--- src/shared/template/GuestTemplate.jsx
+++ src/shared/template/GuestTemplate.jsx
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function GuestTemplate() {
   return (
     <div className="bg-gradient-to-b from-cyan-700 to-gray-400 text-gray-700 w-full flex justify-center">
-      <div className="w-full xl:w-2/3">
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 }
