4ba93135b126243d4b87d90e8d2b3aaf8d93114d
Rocco Lena <roccolena@tuta.io>
2024-02-08 08:39:48 +0100

    .

--- src/Protected.jsx
+++ src/Protected.jsx
@@ -4,8 +4,5 @@ import { Navigate } from "react-router-dom";
 export default function Protected({ children }) {
   const loggedUser = useSelector((state) => state.auth.user);
 
-  if (!loggedUser) {
-    return <Navigate to="/login" replace />;
-  }
-  return children;
+  return loggedUser ? children : <Navigate to="/login" replace />;
 }
