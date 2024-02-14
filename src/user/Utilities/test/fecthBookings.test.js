5ba9c3225722b2a99242a6dca8d79d4e0216123c
Rocco Lena <roccolena@tuta.io>
2024-02-14 15:57:17 +0100

    .

--- src/user/Utilities/test/fecthBookings.test.js
+++ src/user/Utilities/test/fecthBookings.test.js
@@ -17,19 +17,14 @@ test("fetchBookings successfully", async () => {
     { id: "2", name: "Booking 2" },
   ];
 
-  // Mock della chiamata a getListFromDatabase
   getListFromDatabase.mockResolvedValue(mockBookingsFromDatabase);
 
-  // Esegui la funzione fetchBookings
   await fetchBookings(mockDispatch);
 
-  // Verifica che getListFromDatabase sia stata chiamata
   expect(getListFromDatabase).toHaveBeenCalledWith("bookings");
 
-  // Verifica che setBookings sia stata chiamata con i dati restituiti da getListFromDatabase
   expect(setBookings).toHaveBeenCalledWith(mockBookingsFromDatabase);
 
-  // Verifica che dispatch sia stata chiamata con l'azione giusta (setBookings)
   expect(mockDispatch).toHaveBeenCalledWith(setBookings(mockBookingsFromDatabase));
 });
 
@@ -50,28 +45,21 @@ test("fetchBookings with no reservations in the database", async () => {
 test("fetchBookings with error fetching bookings", async () => {
   const mockDispatch = jest.fn();
 
-  // Simula un errore in getListFromDatabase
   const mockError = new Error("Error retrieving reservations");
   getListFromDatabase.mockRejectedValue(mockError);
 
-  // Crea uno spy per console.error
   const consoleErrorSpy = jest.spyOn(console, "error");
 
-  // Esegui la funzione fetchBookings
   await fetchBookings(mockDispatch);
 
-  // Verifica che getListFromDatabase sia stata chiamata
   expect(getListFromDatabase).toHaveBeenCalledWith("bookings");
 
-  // Verifica che dispatch non sia stata chiamata (a causa dell'errore)
   expect(mockDispatch).not.toHaveBeenCalled();
 
-  // Verifica che sia stato loggato un messaggio di errore sulla console
   expect(consoleErrorSpy).toHaveBeenCalledWith(
     "Error retrieving reservations from database:",
     mockError
   );
 
-  // Ripristina lo spy dopo il test
   consoleErrorSpy.mockRestore();
 });
