91ef2b536805c39ff2340750b31122fe23ea05e7
Rocco Lena <roccolena@tuta.io>
2024-02-15 20:35:24 +0100

    .

--- src/user/component/form/validator/UpdatePasswordValidator.jsx
+++ src/user/component/form/validator/UpdatePasswordValidator.jsx
@@ -7,8 +7,8 @@ export const UpdatePasswordValidator = yup.object().shape({
     .min(8, "Password must be at least 8 characters")
     .max(20, "Password is too long")
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\£\$\%\&\/\(\)\=\|\?\^\*\§\°\-\_\.\,\<\;])/,
+      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special characters'
     ),
   newPassword: yup
     .string()
@@ -16,8 +16,7 @@ export const UpdatePasswordValidator = yup.object().shape({
     .min(8, "Password must be at least 8 characters")
     .max(20, "Password is too long")
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\£\$\%\&\/\(\)\=\|\?\^\*\§\°\-\_\.\,\<\;])/,
+      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special characters'
     )
-    .trim(),
 });
