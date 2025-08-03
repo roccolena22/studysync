876c6abce3654a3ccab5093c410b8e9d6a11e66b
Rocco Lena <roccolena@tuta.io>
2025-08-03 17:18:14 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/studysync/",
   build: {
     outDir: "docs",
   },
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
 });
